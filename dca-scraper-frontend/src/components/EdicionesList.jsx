82337a0a9559a2cc681a8975307b3a4c5fba0410
VennusAmery <vennussalguero@gmail.com>
2026-09-23 18:24:25 -0600

    YA EN SERIO

--- dca-scraper-frontend/src/components/EdicionesList.jsx
+++ dca-scraper-frontend/src/components/EdicionesList.jsx
@@ -118,7 +118,7 @@ if (cargando) {
   return (
     <div className="ediciones-list">
       <div className="ediciones-header-fijo">
-        <h1>MiniDCA</h1>
+        <h1> MiniDCA </h1>
 
         <div className="controles-filtro">
           <input
